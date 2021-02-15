8745e86f3802114dc3d0f1f374cbcbe7c7183cc1
YiChun Sung <ycsung.r@gmail.com>
2021-02-15 17:55:22 +0800

    Trying

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
 	res.send('<h1>Hello Docker lalalal yoyoyo</h1>');
 });
 
+app.get('/hello', function(req, res){
+	res.send('<h1>Hello page lalalal yoyoyo</h1>');
+});
+
 let port = 8000;
 
 app.listen(port, function(){
