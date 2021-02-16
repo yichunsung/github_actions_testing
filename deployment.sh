426beac13cd3daf65db124b9cf45e44c53285036
YiChun Sung <ycsung.r@gmail.com>
2021-02-16 17:25:52 +0800

    Added docker-compose

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 echo "Rebuild the image"
 sudo docker build . -t actions-run
 
-echo "Restart container"
-sudo docker stop testing-run && sudo docker rm testing-run && sudo docker run -p 8000:8000 --name testing-run -d actions-run
+#echo "Restart container"
+#sudo docker stop testing-run && sudo docker rm testing-run && sudo docker run -p 8000:8000 --name testing-run -d actions-run
 
