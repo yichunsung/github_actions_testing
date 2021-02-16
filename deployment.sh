675041bd74a48d28d8af98e665a1e5ba78de2991
YiChun Sung <ycsung.r@gmail.com>
2021-02-16 18:03:37 +0800

    Test allow ip

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,8 @@
 echo "Rebuild the image"
 sudo docker build . -t actions-run
 
+echo "Remvoe all old images"
+sudo docker rmi $(sudo docker images -f "dangling=true" -q --no-trunc)
 #echo "Restart container"
 #sudo docker stop testing-run && sudo docker rm testing-run && sudo docker run -p 8000:8000 --name testing-run -d actions-run
 
